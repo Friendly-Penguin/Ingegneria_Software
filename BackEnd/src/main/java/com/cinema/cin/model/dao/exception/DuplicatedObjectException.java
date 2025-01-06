66ae9077d5707a8fa42eb28aac6e39c52d1d255a
Friendly-Penguin <143139550+Friendly-Penguin@users.noreply.github.com>
2025-01-06 16:23:14 +0100

    Delete BackEnd directory

--- BackEnd/src/main/java/com/cinema/cin/model/dao/exception/DuplicatedObjectException.java
+++
@@ -1,6 +0,0 @@
-package com.cinema.cin.model.dao.exception;
-
-public class DuplicatedObjectException extends Exception{
-    public DuplicatedObjectException(){}
-    public DuplicatedObjectException(String s){super(s);};
-}
